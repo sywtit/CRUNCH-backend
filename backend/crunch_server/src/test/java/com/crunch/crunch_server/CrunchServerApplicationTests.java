a0532f9eae503a2a9be309a49bd450e75e2271d8
sywtit <youworthit17@naver.com>
2020-10-05 23:49:58 +0900

    connect springboot and vue.js

---
+++ backend/crunch_server/src/test/java/com/crunch/crunch_server/CrunchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crunch.crunch_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrunchServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
