8cdc5f97c32fad840366995b326f9e8cb5deabe8
hyejin428 <valeria28@ajou.ac.kr>
2020-11-05 13:55:17 +0900

    project domain 작업중

---
+++ backend/crunch_server/src/main/java/com/crunch/crunch_server/domain/project/controller/ProjectController.java
@@ -0,0 +1,23 @@
+// package com.crunch.crunch_server.domain.project.controller;
+package com.crunch.crunch_server.domain.project.controller;
+
+import com.crunch.crunch_server.domain.project.dto.*;
+
+import com.crunch.crunch_server.domain.project.entity.*;
+import com.crunch.crunch_server.domain.uprojectser.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+// Restcontroller는 json 형태로 객체 데이터를 반환 
+// 1. Client가 url형식으로 서버에 요청을 보내면
+// 2. mapping되는 handler와 
+@RequestMapping("/api")
+public class ProjectController{
+
+    @Autowired
+    private ProjectService service;
+
+
+
+
+}
---
+++ backend/crunch_server/src/main/java/com/crunch/crunch_server/domain/project/entity/Project.java
@@ -0,0 +1,10 @@
+package com.crunch.crunch_server.domain.project.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
