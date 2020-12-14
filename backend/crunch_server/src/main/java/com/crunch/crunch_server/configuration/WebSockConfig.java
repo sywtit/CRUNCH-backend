6e0741a1881b3a3bea4f7d96f50f8da810b7e448
sywtit <youworthit17@naver.com>
2020-12-15 02:42:21 +0900

    add to websockconfig

--- backend/crunch_server/src/main/java/com/crunch/crunch_server/configuration/WebSockConfig.java
+++ backend/crunch_server/src/main/java/com/crunch/crunch_server/configuration/WebSockConfig.java
@@ -19,7 +19,7 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer{
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/channel");
+        config.enableSimpleBroker("/channel","/private");
         config.setApplicationDestinationPrefixes("/app");
         config.setUserDestinationPrefix("/private");
     }
