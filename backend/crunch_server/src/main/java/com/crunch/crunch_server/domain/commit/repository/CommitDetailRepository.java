a626e8f48a0284e2b82a56a626140545188f215d
sywtit <youworthit17@naver.com>
2020-12-22 20:59:53 +0900

    blob get post line detail list test:

--- backend/crunch_server/src/main/java/com/crunch/crunch_server/domain/commit/repository/CommitDetailRepository.java
+++ backend/crunch_server/src/main/java/com/crunch/crunch_server/domain/commit/repository/CommitDetailRepository.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommitDetailRepository extends JpaRepository<PostLineDetail,Integer> {
 
-	@Query(value = "SELECT * FROM postLineDetail AS p1, (SELECT lineNum, max(id) AS id FROM postLineDetail GROUP BY lineNum limit :afterPostLength)"
-	+"AS p2  WHERE p1.postId = :postId AND p1.lineNum = p2.lineNum AND p1.id = p2.id"
+	@Query(value = "SELECT * FROM postLineDetail AS p1, (SELECT lineNum, max(id) AS id FROM postLineDetail WHERE postId = :postId GROUP BY lineNum limit :afterPostLength)"
+	+"AS p2 WHERE p1.lineNum = p2.lineNum AND p1.id = p2.id"
 	, nativeQuery = true)
 	List<PostLineDetail> findAllByIdOrderedByLineNum(@Param("postId") int postId, @Param("afterPostLength") int afterPostLength);
 
